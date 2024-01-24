7fa3862b06385533838bc94814145df1454eed2a
yippeeKi <artemio.kka@gmail.com>
2024-01-24 21:58:00 +0700

    add first selenium test

--- python_test_trainig.py
+++
@@ -1,9 +0,0 @@
-def testy_git(x, y):
-    if x < y:
-        return x + y
-    else:
-        return x - y
-
-
-print(testy_git(12, 2))
-print(testy_git(5, 7))
---
+++ test_add_group/test_add_group.py
@@ -0,0 +1,73 @@
+# -*- coding: utf-8 -*-
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+class AppDynamicsJob(unittest.TestCase):
+    def setUp(self):
+        # AppDynamics will automatically override this web driver
+        # as documented in https://docs.appdynamics.com/display/PRO44/Write+Your+First+Script
+        self.driver = webdriver.Firefox()
+        self.driver.implicitly_wait(30)
+        self.base_url = "https://www.google.com/"
+        self.verificationErrors = []
+        self.accept_next_alert = True
+    
+    def test_app_dynamics_job(self):
+        driver = self.driver
+        driver.get("http://localhost/addressbook/")
+        driver.find_element(By.NAME, "user").click()
+        driver.find_element(By.NAME, "user").clear()
+        driver.find_element(By.NAME, "user").send_keys("admin")
+        driver.find_element(By.NAME, "pass").click()
+        driver.find_element(By.NAME, "pass").clear()
+        driver.find_element(By.NAME, "pass").send_keys("secret")
+        driver.find_element(By.XPATH, "//input[@value='Login']").click()
+        driver.find_element(By.LINK_TEXT, "groups").click()
+        driver.find_element(By.NAME, "new").click()
+        driver.find_element(By.NAME, "group_name").click()
+        driver.find_element(By.NAME, "group_name").clear()
+        driver.find_element(By.NAME, "group_name").send_keys("test name")
+        driver.find_element(By.NAME, "group_header").click()
+        driver.find_element(By.NAME, "group_header").clear()
+        driver.find_element(By.NAME, "group_header").send_keys("test logo")
+        driver.find_element(By.NAME, "group_footer").click()
+        driver.find_element(By.NAME, "group_footer").clear()
+        driver.find_element(By.NAME, "group_footer").send_keys("test comment")
+        driver.find_element(By.NAME, "submit").click()
+        driver.find_element(By.LINK_TEXT, "groups").click()
+        driver.find_element(By.LINK_TEXT, "Logout").click()
+    
+    def is_element_present(self, how, what):
+        try: self.driver.find_element(by=how, value=what)
+        except NoSuchElementException as e: return False
+        return True
+    
+    def is_alert_present(self):
+        try: self.driver.switch_to_alert()
+        except NoAlertPresentException as e: return False
+        return True
+
+    def close_alert_and_get_its_text(self):
+        try:
+            alert = self.driver.switch_to_alert()
+            alert_text = alert.text
+            if self.accept_next_alert:
+                alert.accept()
+            else:
+                alert.dismiss()
+            return alert_text
+        finally: self.accept_next_alert = True
+
+    def tearDown(self):
+        # To know more about the difference between verify and assert,
+        # visit https://www.seleniumhq.org/docs/06_test_design_considerations.jsp#validating-results
+        self.assertEqual([], self.verificationErrors)
+
+if __name__ == "__main__":
+    unittest.main()
+
