9b7ea329cd81819b2f83074191916ba51bbcdf80
yippeeKi <artemio.kka@gmail.com>
2024-03-03 22:51:34 +0700

    create intellctual fixture

--- contacts/test/conftest.py
+++ contacts/test/conftest.py
@@ -7,9 +7,18 @@ fixture = None
 @pytest.fixture
 def app(request):
     global fixture
-    fixture = Application()
-    fixture.session.login("admin", "secret")
+    if fixture is None:
+        fixture = Application()
+        fixture.session.login("admin", "secret")
+    else:
+        if not fixture.is_valid():
+            fixture = Application()
+            fixture.session.login("admin", "secret")
+    return fixture
 
+
+@pytest.fixture(scope="session", autouse=True)
+def stop(request):
     def fin():
         fixture.session.logout()
         fixture.close()
