0f8a644a5cddcf282966deb23c555f66495cfa3c
yippeeKi <artemio.kka@gmail.com>
2024-03-10 22:38:39 +0700

    change test_modify_contact

--- contacts/test/test_modify_contact.py
+++ contacts/test/test_modify_contact.py
@@ -2,4 +2,11 @@ from contacts.model.contact_name import ContactName
 
 
 def test_modify_contacts_name(app):
-    app.contacts.modify_first_contact(ContactName("Zuka", "Haripovich", "Garinov", "Hp"))
+    if app.contacts.count() == 0:
+        app.driver.get("http://localhost/addressbook/edit.php")
+        app.contacts.new_contact_name(ContactName("Haur", "Nuit", "Geriy", "HGR"))
+        app.contacts.new_contact_create()
+
+    app.contacts.modify_first_contact(ContactName("Buka", "Haripovich",
+                                                  "Garinov", "Hp"))
+
