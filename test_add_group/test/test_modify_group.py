eb99c91da379e3beb1c9d8a6c768e677f5400329
yippeeKi <artemio.kka@gmail.com>
2024-03-18 16:50:38 +0700

    list comporison for test_modify

--- test_add_group/test/test_modify_group.py
+++ test_add_group/test/test_modify_group.py
@@ -3,9 +3,13 @@ from test_add_group.model.group import Group
 
 def modify_group_name(app):
     old_groups = app.group.get_group_list()
-    app.group.modify_first_group(Group(name="New group"))
-    new_groups = app.group.get_group_list()
+    group = Group(name="New group")
+    app.group.modify_first_group()
+    new_groups = app.group.get_group_list(group)
+    group.id = old_groups[0].id
     assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0] = group
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 def modify_group_header(app):
