48e4338f1776efcb86bec30065c0503f1bdd979f
yippeeKi <artemio.kka@gmail.com>
2024-03-18 16:38:16 +0700

    moved id_or_max in class Group

--- test_add_group/test/test_add_group.py
+++ test_add_group/test/test_add_group.py
@@ -10,13 +10,7 @@ def test_add_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
-
-    def id_or_max(gr):
-        if gr.id:
-            return int(gr.id)
-        else:
-            return maxsize
-    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
 
 
 
