bd67e7b241345cbc811b5ebcea50200dd158070c
yippeeKi <artemio.kka@gmail.com>
2024-03-18 16:32:17 +0700

    sorting list

--- test_add_group/test/test_add_group.py
+++ test_add_group/test/test_add_group.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 from test_add_group.model.group import Group
+from sys import maxsize
 
 
 def test_add_group(app):
@@ -9,6 +10,14 @@ def test_add_group(app):
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
     old_groups.append(group)
+
+    def id_or_max(gr):
+        if gr.id:
+            return int(gr.id)
+        else:
+            return maxsize
+    assert sorted(old_groups, key=id_or_max) == sorted(new_groups, key=id_or_max)
+
 
 
 
