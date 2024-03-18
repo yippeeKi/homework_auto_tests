5c8c446c03d426289ac84e72ce1fec54bd3f2a20
yippeeKi <artemio.kka@gmail.com>
2024-03-18 15:57:18 +0700

    new list comparison in test_del_groups

--- test_add_group/test/test_del_group.py
+++ test_add_group/test/test_del_group.py
@@ -2,9 +2,11 @@ from test_add_group.model.group import Group
 
 
 def test_delete_first_group(app):
-    old_groups = app.group.get_group_list()
     if app.group.count() == 0:
         app.group.create(Group(name="test"))
+    old_groups = app.group.get_group_list()
     app.group.delete_first_group()
     new_groups = app.group.get_group_list()
     assert len(old_groups) - 1 == len(new_groups)
+    old_groups[0:1] = []
+    assert new_groups == new_groups
